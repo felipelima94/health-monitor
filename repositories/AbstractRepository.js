b4be7c206b80c6ab438ffdd489159175915d5eea
Felipe <fe.max@hotmail.com>
2021-09-12 22:44:52 -0300

    feat: implementing new abstract repository

---
+++ repositories/AbstractRepository.js
@@ -0,0 +1,23 @@
+const message = require('./../utils/MessagesHandler')
+
+const create = async (REP, data) => {
+    let msg = message.sorryStatus()
+    await REP.create(data)
+    .then(response => {
+        msg = message.successStatus(response)
+    })
+    .catch(err => {
+        msg = message.errorStatus(err)
+    })
+    return msg
+}
+
+const find = (REP, filter) => {
+    const query = REP.find(filter)
+    return query.exec()
+}
+
+module.exports = {
+    create,
+    find
+}
