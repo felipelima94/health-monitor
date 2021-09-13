c083a4a9bc29243589b056ed88c1e27b8f5a11c4
Felipe <fe.max@hotmail.com>
2021-09-12 22:50:02 -0300

    refactor: url resource refactored to consume the repository

--- routes/UrlResource.js
+++ routes/UrlResource.js
@@ -1,17 +1,14 @@
 const express = require('express')
-const Url = require('../models/Url')
-const message = require('../utils/Messages')
+const urlRepository = require('./../repositories/UrlRepository')
 
 let router = express.Router()
 
-router.get('/create', (req, res) => {
-    Url.create(req.body)
-    .then(url => {
-        res.json(message.successStatus(url))
-    })
-    .catch(err => {
-        res.json(message.errorStatus(err))
-    })
-})
+router.post('/create', async (req, res) => {
+        res.json( await urlRepository.create(req.body) )
+    }
+).get('/find', async (req, res) => {
+        res.json( await urlRepository.find({}) )
+    }
+)
 
 module.exports = router
