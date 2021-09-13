f9b5c7521a91d2cd18b62b91eb3153e9293c2fce
Felipe <fe.max@hotmail.com>
2021-09-12 22:43:35 -0300

    fix: url model field and add new field

--- models/Url.js
+++ models/Url.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose')
 const Url = new mongoose.Schema({
     name: { type: String },
     url: { type: String },
-    categorys: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Category' }],
-    tags: { type: mongoose.Schema.Types.ObjectId, ref: 'Tag' },
+    category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category' },
+    tags: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Tag' }],
+    cron: { type: Number, required: true },
     last_ok: { type: Date },
     last_check: { type: Date, default: Date.now }
 })
